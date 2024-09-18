8f79354bacc549862b4197803fe12a82816eeedd
gui2one <guillaume.rouault.fx@gmail.com>
2024-09-18 11:05:33 +0200

    added GLM ...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
 int main() {
+    glm::vec3 pos = glm::vec3(); 
     std::cout << "Hello" << std::endl;
+    std::cout << glm::to_string(pos) << std::endl;
     return 0;
 }
