e81730f4cb34c1e7152c2e51107dbbc0d779d811
gui2one <guillaume.rouault.fx@gmail.com>
2024-10-04 23:24:49 +0200

    still implementing node_editor

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "cpp_mesher.h"
-#include "meshop/generators/SquareGenerator.h"
-#include "meshop/generators/GridGenerator.h"
-#include "meshop/generators/CylinderGenerator.h"
-#include "meshop/modifiers/NormalModifier.h"
-#include "meshop/modifiers/TransformModifier.h"
+// #include "meshop/generators/SquareGenerator.h"
+// #include "meshop/generators/GridGenerator.h"
+// #include "meshop/generators/CylinderGenerator.h"
+// #include "meshop/modifiers/NormalModifier.h"
+// #include "meshop/modifiers/TransformModifier.h"
+// #include "OperatorNetwork.h"
+
 #include "MeshImporter.h"
 #include "MeshExporter.h"
-#include "OperatorNetwork.h"
-
 #include "utils.h"
 #include "mesh_utils.h"
 
@@ -16,8 +16,10 @@
 #include "formatters.h"
 
 #include "node_editor.h"
-using namespace msh;
+#include "nodes/MeshOperators.h"
 
+using namespace msh;
+using namespace NodeEditor;
 MeshExporter me;
 void export_temp_mesh(Mesh& mesh){
     fs::path path = fs::temp_directory_path() / "temp_mesh.ply";
@@ -35,44 +37,75 @@ int main() {
         
         return -1;
     }
+
+    auto& manager = app.GetNodeManager();
+
+
+    auto null_node = std::make_shared<Node<NullMeshOperator>>("Null");
+    null_node->position = ImVec2(650, 600);
+    manager.AddNode(null_node);
+
+    manager.SetNodesMenu([&manager](){
+      if (ImGui::MenuItem("Generator", NULL, false, true)) {
+        manager.AddNode(std::make_shared<Node<SquareGenerator>>("Square"));
+      }
+      if (ImGui::MenuItem("Concatenator", NULL, false, true)) {
+        manager.AddNode(
+            std::make_shared<Node<MeshMerger>>("Concatenator"));
+      }
+      if (ImGui::MenuItem("Repeater", NULL, false, true)) {
+        manager.AddNode(std::make_shared<Node<GridGenerator>>("Grid"));
+      }
+    });
+
+    EventManager::GetInstance().Subscribe(
+      EventType::NodeConnection, [&app, &null_node](const Event &event) {
+        auto &manager = app.GetNodeManager();
+        manager.Evaluate();
+        auto op = static_cast<MeshOperator *>(manager.GetOutputNode().get());
+        std::cout << "Connection Update -> " << op->m_MeshCache << std::endl;
+      });
+
+    manager.SetOutputNode(null_node);
+    app.GetNodeManager().Evaluate();
     app.Run();
 
-    OperatorNetwork net; 
-    Ref<GridGenerator> grid = MakeRef<GridGenerator>(5.0f, 5.0f, 50, 10);
+    // OperatorNetwork net; 
+    // Ref<GridGenerator> grid = MakeRef<GridGenerator>(5.0f, 5.0f, 50, 10);
     
-    grid->setName("Grid Operator");
-    Ref<CylinderGenerator> cylinder = MakeRef<CylinderGenerator>();
-    cylinder->setName("Cylinder Operator");
-    cylinder->mRadius = 2.0f;
-    cylinder->mHeight = 5.0f;
+    // grid->setName("Grid Operator");
+    // Ref<CylinderGenerator> cylinder = MakeRef<CylinderGenerator>();
+    // cylinder->setName("Cylinder Operator");
+    // cylinder->mRadius = 2.0f;
+    // cylinder->mHeight = 5.0f;
 
-    net.addOperator(cylinder);
+    // net.addOperator(cylinder);
     
-    Ref<NormalModifier> normal_mod = MakeRef<NormalModifier>();
-    normal_mod->setName("Normal Modifier");
-    normal_mod->setInput(0, net.getOperators()[0]);
-    net.addOperator(normal_mod);
+    // Ref<NormalModifier> normal_mod = MakeRef<NormalModifier>();
+    // normal_mod->setName("Normal Modifier");
+    // normal_mod->setInput(0, net.getOperators()[0]);
+    // net.addOperator(normal_mod);
 
 
-    Ref<TransformModifier> transform_mod = MakeRef<TransformModifier>();
-    transform_mod->setName("Transform Modifier");
-    transform_mod->setInput(0, net.getOperators()[1]);
+    // Ref<TransformModifier> transform_mod = MakeRef<TransformModifier>();
+    // transform_mod->setName("Transform Modifier");
+    // transform_mod->setInput(0, net.getOperators()[1]);
 
-    transform_mod->mTransformOrder = meshutils::TRANSFORM_ORDER::TRS;
-    transform_mod->mRot = glm::vec3(0.0f, PI / 2.0f, 0.0f);
+    // transform_mod->mTransformOrder = meshutils::TRANSFORM_ORDER::TRS;
+    // transform_mod->mRot = glm::vec3(0.0f, PI / 2.0f, 0.0f);
 
-    net.addOperator(transform_mod);
-    net.evaluate();
+    // net.addOperator(transform_mod);
+    // net.evaluate();
 
-    Mesh result = net.getOperators()[net.getOperators().size() - 1]->mMeshCache;
+    // Mesh result = net.getOperators()[net.getOperators().size() - 1]->mMeshCache;
 
 
-    result = meshutils::generateTube2(1.0f, 5.0f, 40, 5);
-    result.ComputeNormals();
+    // result = meshutils::generateTube2(1.0f, 5.0f, 40, 5);
+    // result.ComputeNormals();
 
-    LOG_INFO("result : {}", result);
+    // LOG_INFO("result : {}", result);
 
-    export_temp_mesh(result);
+    // export_temp_mesh(result);
 
     std::cout << "All Done !!" << std::endl;
     
---
+++ src/nodes/MeshOperators.h
@@ -0,0 +1,151 @@
+#ifndef MESH_OPERATORS_H
+#define MESH_OPERATORS_H
+#pragma once
+
+#include "ImguiNode.h"
+#include "NodeParam.h"
+#include <type_traits>
+#include <iostream>
+#include <string>
+#include <stdint.h>
+
+#include "mesh.h"
+#include "mesh_utils.h"
+namespace NodeEditor {
+
+class MeshOperator : public ImGuiNode{
+public:
+    MeshOperator(): ImGuiNode("default"){};
+    virtual ~MeshOperator() = default;
+    virtual void Generate() = 0;
+
+public :
+    msh::Mesh m_MeshCache;
+
+    // std::array<MeshOperator*, 4> inputs = { nullptr, nullptr, nullptr, nullptr }; 
+};
+
+class MeshGenerator : public MeshOperator
+{
+public:
+    MeshGenerator():MeshOperator(){
+        SetNumAvailableInputs(0);
+    };
+    ~MeshGenerator(){};
+    virtual void Generate() = 0;
+private:
+
+};
+
+class SquareGenerator : public MeshGenerator
+{
+public:
+    SquareGenerator():MeshGenerator(){};
+    ~SquareGenerator(){};
+
+    void Generate() override{
+        m_MeshCache = msh::meshutils::generateGrid(1.0f, 1.0f, 1, 1);
+    }
+private:
+};
+
+
+class GridGenerator : public MeshGenerator
+{
+public:
+    GridGenerator():MeshGenerator(){};
+    ~GridGenerator(){};
+
+    void Generate() override{
+        m_MeshCache = msh::meshutils::generateGrid(5.0f, 5.0f, 32, 32);
+    }
+private:
+};
+
+class MeshModifier : public MeshOperator
+{
+public:
+    MeshModifier():MeshOperator(){};
+    ~MeshModifier(){};
+    virtual void Generate() = 0;
+private:
+
+};
+
+class MeshMerger : public MeshModifier
+{
+public:
+    MeshMerger() : MeshModifier() {
+        SetNumAvailableInputs(2);
+    };
+    ~MeshMerger(){};
+
+    void Generate() override{
+        if( GetInput(0) != nullptr && GetInput(1) != nullptr) {
+            auto op0 = static_cast<MeshOperator*>(GetInput(0).get());
+            auto op1 = static_cast<MeshOperator*>(GetInput(1).get());
+            m_MeshCache = msh::meshutils::merge(op0->m_MeshCache, op1->m_MeshCache);
+        }
+    }
+};
+
+
+class NullMeshOperator : public MeshModifier{
+public:
+    NullMeshOperator():MeshModifier(){
+        SetNumAvailableInputs(1);
+    };
+    ~NullMeshOperator(){};
+
+    void Generate() override{
+        if( GetInput(0) != nullptr) {
+            auto op0 = static_cast<MeshOperator*>(GetInput(0).get());
+            m_MeshCache = op0->m_MeshCache;
+        }
+    }
+};
+
+template <typename T>
+class Node : public T
+{
+    static_assert(std::is_base_of<MeshOperator, T>::value, "T must be derived from MeshOperator");
+public:
+    Node(const char * _title) {
+        auto node = static_cast<ImGuiNode*>(this);
+        if(std::is_base_of<MeshGenerator, T>::value) {
+            node->color = NODE_COLOR::MAROON;
+            node->title = _title;
+        }else if(std::is_base_of<NullMeshOperator, T>::value) {
+            node->color = NODE_COLOR::ORANGE;
+            node->title = _title;
+        }else if(std::is_base_of<MeshModifier, T>::value) {
+            node->color = NODE_COLOR::DARK_GREEN;
+            node->title = _title;
+        }
+    }
+
+    void Update() {
+        auto node = static_cast<ImGuiNode*>(this);
+        auto op = static_cast<MeshOperator*>(this);
+        for(uint32_t i = 0; i < MAX_N_INPUTS; i++) {
+            if(node->GetInput(i) != nullptr) {
+                node->GetInput(i)->Update(); /* Important !!*/
+                auto opinput = static_cast<MeshOperator*>(node->GetInput(i).get());
+                op->SetInput(i, node->GetInput(i));
+                opinput->Generate();
+                // std::cout << node->title << " -> " << op->inputs[i]->m_MeshCache << std::endl;
+            }
+        }
+        op->Generate();
+
+        // 'final' output
+        // std::cout << op->m_MeshCache << std::endl;
+    }
+
+    T* ToOperator() {
+        return static_cast<T*>(this);
+    }
+};
+};
+
+#endif
