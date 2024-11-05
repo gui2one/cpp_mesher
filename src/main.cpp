23fd88928ee423e366868e9339a613639000607c
gui2one <guillaume.rouault.fx@gmail.com>
2024-11-05 01:01:26 +0100

    renamed main_new.cpp to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,136 @@
-// #include "cpp_mesher.h"
+#include <iostream>
 
-// #include "MeshImporter.h"
-// #include "MeshExporter.h"
-// #include "utils.h"
-// #include "mesh_utils.h"
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <imgui.h>
 
-// #include "gltf_utils.h"
-// #include "Mesh.h"
-// #include "formatters.h"
+#include "Mesh.h"
+#include "MeshExporter.h"
 
-// #include "node_editor.h"
-// #include "nodes/MeshOperators.h"
-// // #include "nodes/node_params.h"
+#include "node_editor.h"
+#include "nodes/MeshOperators.h"
+// #include "nodes/node_params.h"
 
+#include "Log.h"
+#include "cpp_mesher.h"
 
-
-// using namespace msh;
-// using namespace NodeEditor;
-
-// void export_temp_mesh(Mesh& mesh){
-//     fs::path path = fs::temp_directory_path() / "temp_mesh.ply";
-//     MeshExporter me;
-//     me.MakeScene(mesh);
-//     me.ExportPLY(path.string().c_str());
-// }
-
-// int main() {
+#include "Application.h"
 
 
-  
-//   Log::Init();
+using namespace msh;
+using namespace NodeEditor;
+void add_example_nodes(NodeManager &manager);
+int main(){
 
-//   Application app;
-//   if(!app.Init()){
-//       std::cout << "Problem !!!" << std::endl;
-      
-//       return -1;
-//   }
+    Log::Init();
 
-//   auto& manager = app.GetNodeManager();
+    // auto tube = meshutils::generateTube(1.0f, 1.0f, 1.0f, 25, 2);
+    // meshutils::fusePoints(tube, 0.005f);
 
+    // return 0;
+    REGISTER_NODE_TYPE(NodeEditor::GridGenerator, "Grid", "Generators");
+    REGISTER_NODE_TYPE(NodeEditor::TubeGenerator, "Tube", "Generators");
+    REGISTER_NODE_TYPE(NodeEditor::SquareGenerator, "Square", "Generators");
 
-//   auto grid = std::make_shared<Node<GridGenerator>>("Grid");
-//   grid->position = ImVec2(650, 500);
-//   manager.AddNode(grid);
-//   auto noise_node = std::make_shared<Node<NoiseDisplaceModifier>>("Noise Displace");
-//   noise_node->position = grid->position + ImVec2(0, 100);
-//   noise_node->SetInput(0, grid);
-//   manager.AddNode(noise_node);
-//   auto null_node = std::make_shared<Node<NullMeshOperator>>("Null");
-//   null_node->position = noise_node->position + ImVec2(0, 100);
-//   null_node->SetInput(0, noise_node);
-//   manager.AddNode(null_node);
+    REGISTER_NODE_TYPE(NodeEditor::NormalModifier, "Normal", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::TransformModifier, "Transform", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::NoiseDisplaceModifier, "Noise Displace", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::MeshMerger, "Merge", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::MeshMergerMulti, "Merge Multi", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::MeshTwister, "Twister", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::MeshCenter, "Center", "Modifiers");
+    REGISTER_NODE_TYPE(NodeEditor::FusePoints, "Fuse Points", "Modifiers");
 
-//   // manager.SetNodesMenu([&manager](){
-//   //   if(ImGui::BeginMenu("Generators")){
+    REGISTER_NODE_TYPE(NodeEditor::NullMeshOperator, "Null", "Utility");
+    REGISTER_NODE_TYPE(NodeEditor::MeshSubnetOperator, "Subnet", "Utility");
 
-//   //     node_menu_item<Node<SquareGenerator>>(manager,"Square");
-//   //     node_menu_item<Node<GridGenerator>>(manager,"Grid");
-//   //     node_menu_item<Node<TubeGenerator>>(manager,"Tube");
-//   //     ImGui::EndMenu();
-//   //   }
+    CREATE_SUBNET_INPUT_NODE_CLASS(msh::Mesh, "Subnet input", "utility");
 
-//   //   if(ImGui::BeginMenu("Modifiers")){
-//   //     node_menu_item<Node<NormalModifier>>(manager,"Normal");
-//   //     node_menu_item<Node<TransformModifier>>(manager,"Transform");
-//   //     node_menu_item<Node<NoiseDisplaceModifier>>(manager,"Noise Displace");
-//   //     ImGui::EndMenu();
-//   //   }
-//   //   node_menu_item<Node<MeshMerger>>(manager,"Merge");
+    msh::Application app;
+    app.Init();
 
-//   // });
-
-//   EventManager::GetInstance().Subscribe(
-//     EventType::NodeConnection, [&app](const Event &event) {
-//       auto &manager = app.GetNodeManager();
-//       manager.Evaluate();
-//       auto op = static_cast<MeshOperator *>(manager.GetOutputNode().get());
-//       std::cout << "Connection Update -> " << op->m_MeshCache << std::endl;
-//       export_temp_mesh(op->m_MeshCache);
-//     });
-//   EventManager::GetInstance().Subscribe(
-//     EventType::ParamChanged, [&app](const Event &event) {
-//       auto &manager = app.GetNodeManager();
-//       manager.Evaluate();
-//       auto op = static_cast<MeshOperator *>(manager.GetOutputNode().get());
-//       std::cout << "ParamChanged Event -> " << op->m_MeshCache << std::endl;
-//       export_temp_mesh(op->m_MeshCache);
-//     });
+    NodeManager& manager = app.GetNodeManager();
 
 
-//   manager.SetOutputNode(null_node);
-//   app.GetNodeManager().Evaluate();
-//   app.Run();
+  // add_example_nodes(manager);
 
-//   std::cout << "All Done !!" << std::endl;
-  
-//   return 0;
-// }
+  static EventDispatcher& dispatcher = EventManager::GetInstance();
 
+  dispatcher.Subscribe(
+      EventType::NodeConnection, [&app](const Event &event) {
+        auto &manager = app.GetNodeManager();
+        manager.Evaluate();
+        if(manager.GetOutputNode() != nullptr){
+          auto op = static_cast<MeshOperator *>(manager.GetOutputNode().get());
+          std::cout << "Connection Update -> " << op->m_DataCache << std::endl;
+          dispatcher.Dispatch(ManagerUpdateEvent());
+        }
+      });
+  dispatcher.Subscribe(
+      EventType::ParamChanged, [&app](const Event &event) {
+        auto &manager = app.GetNodeManager();
+        manager.m_OneParamChanged = true;
+      });
+  dispatcher.Subscribe(
+      EventType::ManagerUpdate, [&app](const Event &event) {
+        auto &manager = app.GetNodeManager();
+        manager.Evaluate();
+        if(manager.GetOutputNode() != nullptr){
+          auto subnet_op = std::dynamic_pointer_cast<MeshSubnetOperator>(manager.GetOutputNode());
+          auto subnet_input_op = std::dynamic_pointer_cast<SubnetInputNode<msh::Mesh>>(manager.GetOutputNode());
+          auto op = std::dynamic_pointer_cast<ImGuiNode<msh::Mesh>>(manager.GetOutputNode());
+          if(subnet_op != nullptr){
+            if(subnet_op->node_network.outuput_node != nullptr){
+              auto output_op = std::dynamic_pointer_cast<ImGuiNode<msh::Mesh>>(subnet_op->node_network.outuput_node);
+              std::cout << "Want Subnet Data Cache !!!!!!!!" << std::endl;
+              std::cout << "m_DataCache -> " << output_op->m_DataCache << std::endl;
+              
+            }
+          }else if(op != nullptr){
+            std::cout << "ManagerUpdate Event -> " << op->m_DataCache << std::endl;
+            app.ExportTempMesh();
+          }else if(subnet_input_op != nullptr){
+            auto op2 = static_cast<ImGuiNode<msh::Mesh>*>(subnet_input_op->parent_node->GetInput(0).get());
+            std::cout << "Subnet input Operator -> " <<  op2->m_DataCache << std::endl;
+            
+          }else{
+            std::cout << "can't convert to Operator" << std::endl;
+          }
+        }
+
+      });
+
+    app.Run();
+
+    std::cout << "__ALL_DONE__ " << std::endl;
+    
+    return 0;
+}
+
+void add_example_nodes(NodeManager &manager){
+
+    auto grid_node = std::make_shared<Node<GridGenerator>>("Grid");
+    grid_node->position = ImVec2(0,0);
+    grid_node->width->value = 1.0f;
+    grid_node->length->value = 1.0f;
+    grid_node->cols->value = 100;
+    grid_node->rows->value = 100;
+    manager.AddNode(grid_node);
+
+    auto noise_node = std::make_shared<Node<NoiseDisplaceModifier>>("Noise Displace");
+    noise_node->position = grid_node->position + ImVec2(0, 100);
+    noise_node->SetInput(0, grid_node);
+    manager.AddNode(noise_node);
+
+    auto normal_node = std::make_shared<Node<NormalModifier>>("Normal");
+    normal_node->position = noise_node->position + ImVec2(0, 100);
+    normal_node->SetInput(0, noise_node);
+    manager.AddNode(normal_node);
+
+    auto output_node = std::make_shared<Node<NullMeshOperator>>("Null");
+    output_node->position = normal_node->position + ImVec2(0, 100);
+    output_node->SetInput(0, normal_node);
+    manager.AddNode(output_node);
+
+    manager.SetOutputNode(output_node);
+}
