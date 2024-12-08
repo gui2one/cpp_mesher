cd47b5414f8309c27c2bfd34f5b1584a0681e7d6
gui2one <guillaume.rouault.fx@gmail.com>
2024-12-08 23:09:39 +0100

    drawing ParamFloatRamp line

--- src/custom_params.h
+++ src/custom_params.h
@@ -64,26 +64,18 @@ class ParamFloatRamp : public Param<FloatRamp> {
   int current_point = -1;
 
  public:
-  // ParamFloatRamp() = default;
-  // ParamFloatRamp(const ParamFloatRamp& other) { std::cout << "copy constructor called" << std::endl; }
-  //~ParamFloatRamp() { std::cout << "destructor called" << std::endl; }
   void Init() {
     AddPoint(0.0f, 0.0f);
     AddPoint(1.0f, 1.0f);
   }
 
-  void Set(FloatRamp _value) {
-    std::cout << "Setting ParamFloatRamp value " << std::endl;
-    value = _value;
-    value.points = _value.points;
-    temp_value = _value;
-    temp_value.points = _value.points;
-    SortPoints();
-  }
-
-  void AddPoint(float pos, float val) {
+  void AddPoint(float pos, float val, bool trigger_event = false) {
     temp_value.points.push_back({pos, val});
-    // SortPoints();
+    if (trigger_event) {
+      ParamChangedEvent<FloatRamp> event(m_Node, m_Label, temp_value, value);
+      EventManager::GetInstance().Dispatch(event);
+    }
+    SortPoints();
     value = temp_value;
   }
 
@@ -96,6 +88,15 @@ class ParamFloatRamp : public Param<FloatRamp> {
       draw_list->AddRectFilled(canvas_p0, canvas_p0 + ImVec2(ImGui::GetContentRegionAvail().x, height),
                                IM_COL32(50, 50, 50, 255));
 
+      for (size_t i = 0; i < temp_value.points.size() - 1; i++) {
+        auto cur_pt = temp_value.points[i];
+        auto next_pt = temp_value.points[i + 1];
+        float x1 = (cur_pt.pos * ImGui::GetContentRegionAvail().x) + canvas_p0.x;
+        float y1 = (1.0f - cur_pt.val) * height + canvas_p0.y;
+        float x2 = (next_pt.pos * ImGui::GetContentRegionAvail().x) + canvas_p0.x;
+        float y2 = (1.0f - next_pt.val) * height + canvas_p0.y;
+        draw_list->AddLine(ImVec2(x1, y1), ImVec2(x2, y2), IM_COL32(255, 255, 255, 255));
+      }
       // draw ramp points
       auto pt_color = IM_COL32(0, 255, 255, 255);
       auto cur_pt_color = IM_COL32(255, 255, 0, 255);
@@ -116,8 +117,7 @@ class ParamFloatRamp : public Param<FloatRamp> {
       }
 
       if (ImGui::Button("add point")) {
-        AddPoint(0.5f, 0.0f);
-        TriggerChangeEvent();
+        AddPoint(0.5f, 0.0f, true);
       }
       if (current_point >= 0 && current_point < temp_value.points.size()) {
         ImGui::SliderFloat("Pos", &temp_value.points[current_point].pos, 0.0f, 1.0f, "%.3f");
@@ -133,12 +133,6 @@ class ParamFloatRamp : public Param<FloatRamp> {
         }
       }
       ImGui::Separator();
-      // for (const auto& pt : value.points) {
-
-      //  float x = (pt.pos * ImGui::GetContentRegionAvail().x) + canvas_p0.x;
-      //  float y = (1.0f - pt.val) * height + canvas_p0.y;
-      //  draw_list->AddCircleFilled(ImVec2(x, y), 5.0f, IM_COL32(0, 255, 255, 255));
-      //}
     });
   }
 
