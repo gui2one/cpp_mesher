20a05d54121cb4da08b28b5d9b4f62e0fc8c4b5c
gui2one <guillaume.rouault.fx@gmail.com>
2024-12-13 17:37:28 +0100

    started GMesh import

--- src/MeshImporter.cpp
+++ src/MeshImporter.cpp
@@ -4,102 +4,157 @@ namespace msh {
 MeshImporter* MeshImporter::s_Instance = nullptr;
 
 MeshImporter* MeshImporter::GetInstance() {
-	if (s_Instance == nullptr) {
-		s_Instance = new MeshImporter();
-	}
+  if (s_Instance == nullptr) {
+    s_Instance = new MeshImporter();
+  }
 
-	return s_Instance;
-}
-// MeshImporter::MeshImporter()
-// {
-// 	//LOG_INFO("MeshImporter Singleton created.");
-// }
-
-Mesh MeshImporter::Import(const char *path)
-{
-
-	Assimp::Importer Importer;
-    const aiScene* scene = Importer.ReadFile(path, aiProcess_Triangulate | aiProcess_ForceGenNormals);
-	LOG_INFO("loading mesh file : {}", path);
-	
-	if (scene) {
-
-		std::vector<Point> points;
-		std::vector<Vertex> vertices;
-
-		Mesh imported_mesh;
-		for(size_t mesh_id=0; mesh_id < scene->mNumMeshes; mesh_id++){
-			auto& cur_mesh = scene->mMeshes[mesh_id];
-			for (size_t i = 0; i < cur_mesh->mNumVertices; i++) {
-
-				Point point;
-				Vertex vtx;
-				vtx.point_id = (uint32_t)i;
-				auto& mesh = cur_mesh;
-				point.position.x = mesh->mVertices[i].x;
-				point.position.y = mesh->mVertices[i].y;
-				point.position.z = mesh->mVertices[i].z;
-				
-				if (cur_mesh->HasNormals()) {
-					point.normal.x = mesh->mNormals[i].x;
-					point.normal.y = mesh->mNormals[i].y;
-					point.normal.z = mesh->mNormals[i].z;
-
-					// invert normal ....?!
-					//point.normal *= -1.0f;
-				}
-
-				if (cur_mesh->HasTextureCoords(0)) {
-
-
-					point.t_coords.x = cur_mesh->mTextureCoords[0][i].x;
-					point.t_coords.y = cur_mesh->mTextureCoords[0][i].y;
-
-				}
-				else {
-					point.t_coords.x = point.position.x;
-					point.t_coords.y = point.position.z;
-				}
-				points.push_back(point);
-				vertices.push_back(vtx);
-
-
-			}
-
-			std::vector<Face> faces;
-			for (size_t i = 0; i < cur_mesh->mNumFaces; i++)
-			{
-				std::vector<uint32_t> vertices_index;
-
-				for (size_t j = 0; j < cur_mesh->mFaces[i].mNumIndices; j++)
-				{
-
-					uint32_t index = cur_mesh->mFaces[i].mIndices[j];
-					
-					vertices_index.push_back(index);
-
-				}
-
-				faces.push_back(Face(vertices_index));
-			}
-
-
-			Mesh mesh;
-			mesh.SetPoints(points);
-			mesh.SetVertices(vertices);
-			mesh.SetFaces(faces);
-
-			points.clear();
-			faces.clear();
-
-			imported_mesh = meshutils::merge(imported_mesh, mesh);
-		}
-		return imported_mesh;
-	}
-	// LOG_ERROR("problem loading {0}", path);
-	Mesh empty_mesh;
-	return empty_mesh;
-
+  return s_Instance;
 }
 
+Mesh MeshImporter::Import(const char* path) {
+  Assimp::Importer Importer;
+  const aiScene* scene = Importer.ReadFile(path, aiProcess_Triangulate | aiProcess_ForceGenNormals);
+  LOG_INFO("loading mesh file : {}", path);
+
+  if (scene) {
+    std::vector<Point> points;
+    std::vector<Vertex> vertices;
+
+    Mesh imported_mesh;
+    for (size_t mesh_id = 0; mesh_id < scene->mNumMeshes; mesh_id++) {
+      auto& cur_mesh = scene->mMeshes[mesh_id];
+      for (size_t i = 0; i < cur_mesh->mNumVertices; i++) {
+        Point point;
+        Vertex vtx;
+        vtx.point_id = (uint32_t)i;
+        auto& mesh = cur_mesh;
+        point.position.x = mesh->mVertices[i].x;
+        point.position.y = mesh->mVertices[i].y;
+        point.position.z = mesh->mVertices[i].z;
+
+        if (cur_mesh->HasNormals()) {
+          point.normal.x = mesh->mNormals[i].x;
+          point.normal.y = mesh->mNormals[i].y;
+          point.normal.z = mesh->mNormals[i].z;
+
+          // invert normal ....?!
+          // point.normal *= -1.0f;
+        }
+
+        if (cur_mesh->HasTextureCoords(0)) {
+          point.t_coords.x = cur_mesh->mTextureCoords[0][i].x;
+          point.t_coords.y = cur_mesh->mTextureCoords[0][i].y;
+
+        } else {
+          point.t_coords.x = point.position.x;
+          point.t_coords.y = point.position.z;
+        }
+        points.push_back(point);
+        vertices.push_back(vtx);
+      }
+
+      std::vector<Face> faces;
+      for (size_t i = 0; i < cur_mesh->mNumFaces; i++) {
+        std::vector<uint32_t> vertices_index;
+
+        for (size_t j = 0; j < cur_mesh->mFaces[i].mNumIndices; j++) {
+          uint32_t index = cur_mesh->mFaces[i].mIndices[j];
+
+          vertices_index.push_back(index);
+        }
+
+        faces.push_back(Face(vertices_index));
+      }
+
+      Mesh mesh;
+      mesh.SetPoints(points);
+      mesh.SetVertices(vertices);
+      mesh.SetFaces(faces);
+
+      points.clear();
+      faces.clear();
+
+      imported_mesh = meshutils::merge(imported_mesh, mesh);
+    }
+    return imported_mesh;
+  }
+  // LOG_ERROR("problem loading {0}", path);
+  Mesh empty_mesh;
+  return empty_mesh;
 }
+
+GMesh MeshImporter::GMeshImport(const char* path) {
+  Assimp::Importer Importer;
+  const aiScene* scene = Importer.ReadFile(path, aiProcess_Triangulate | aiProcess_ForceGenNormals);
+  LOG_INFO("loading mesh file : {}", path);
+
+  if (scene) {
+    std::vector<GMesh::Point> points;
+
+    GMesh imported_mesh;
+    for (size_t mesh_id = 0; mesh_id < scene->mNumMeshes; mesh_id++) {
+      auto& cur_mesh = scene->mMeshes[mesh_id];
+      for (size_t i = 0; i < cur_mesh->mNumVertices; i++) {
+        GMesh::Point point;
+        auto& mesh = cur_mesh;
+        float x = mesh->mVertices[i].x;
+        float y = mesh->mVertices[i].y;
+        float z = mesh->mVertices[i].z;
+        point[0] = x;
+        point[1] = y;
+        point[2] = z;
+        // if (cur_mesh->HasNormals()) {
+        //   point.normal.x = mesh->mNormals[i].x;
+        //   point.normal.y = mesh->mNormals[i].y;
+        //   point.normal.z = mesh->mNormals[i].z;
+
+        //  // invert normal ....?!
+        //  // point.normal *= -1.0f;
+        //}
+
+        // if (cur_mesh->HasTextureCoords(0)) {
+        //   point.t_coords.x = cur_mesh->mTextureCoords[0][i].x;
+        //   point.t_coords.y = cur_mesh->mTextureCoords[0][i].y;
+
+        //} else {
+        //  point.t_coords.x = point.position.x;
+        //  point.t_coords.y = point.position.z;
+        //}
+
+        points.push_back(point);
+      }
+
+      for (const auto& pt : points) {
+        imported_mesh.add_vertex(pt);
+      }
+      // std::vector<Face> faces;
+      // for (size_t i = 0; i < cur_mesh->mNumFaces; i++) {
+      //   std::vector<uint32_t> vertices_index;
+
+      //  for (size_t j = 0; j < cur_mesh->mFaces[i].mNumIndices; j++) {
+      //    uint32_t index = cur_mesh->mFaces[i].mIndices[j];
+
+      //    vertices_index.push_back(index);
+      //  }
+
+      //  faces.push_back(Face(vertices_index));
+      //}
+
+      // Mesh mesh;
+      // mesh.SetPoints(points);
+      // mesh.SetVertices(vertices);
+      // mesh.SetFaces(faces);
+
+      // points.clear();
+      // faces.clear();
+
+      // imported_mesh = meshutils::merge(imported_mesh, mesh);
+    }
+    return imported_mesh;
+  }
+  // LOG_ERROR("problem loading {0}", path);
+  GMesh empty_mesh;
+  return empty_mesh;
+}
+
+}  // namespace msh
--- src/MeshImporter.h
+++ src/MeshImporter.h
@@ -1,20 +1,22 @@
 #pragma once
 
 #include <assimp/Importer.hpp>
+
 #include "Mesh.h"
 #include "mesh_utils.h"
+#include "openmesh/gmesh.h"
 
 namespace msh {
-	class MeshImporter {
-	public:
-		Mesh Import(const char* path);
+class MeshImporter {
+ public:
+  Mesh Import(const char* path);
+  GMesh GMeshImport(const char* path);
 
-		static MeshImporter* s_Instance;
-		static MeshImporter* GetInstance();
-	private:
+  static MeshImporter* s_Instance;
+  static MeshImporter* GetInstance();
 
-	protected:
-		MeshImporter() = default;
-
-	};
-}
+ private:
+ protected:
+  MeshImporter() = default;
+};
+}  // namespace msh
