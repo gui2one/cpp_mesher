3971fd336f1aa161785ed0c64fec6165e073643f
gui2one <guillaume.rouault.fx@gmail.com>
2024-12-13 16:46:15 +0100

    back to a more standard openmesh class

--- src/openmesh/gmesh.h
+++ src/openmesh/gmesh.h
@@ -8,26 +8,26 @@ class GMesh : public OpenMesh::PolyMesh_ArrayKernelT<> {
   GMesh() : OpenMesh::PolyMesh_ArrayKernelT<>() {}
 
  public:
-  // Separate container for points
-  std::vector<OpenMesh::VertexHandle> g_points;
-  OpenMesh::PolyMesh_ArrayKernelT<> points_handler;
+  //// Separate container for points
+  // std::vector<OpenMesh::VertexHandle> g_points;
+  // OpenMesh::PolyMesh_ArrayKernelT<> points_handler;
 
-  // Add a point to your custom structure
-  size_t add_point(OMesh::Point point) {
-    // g_points.push_back(point);
-    points_handler.add_vertex(point);
-    return points_handler.n_vertices() - 1;  // Return the index of the added point
-  }
+  //// Add a point to your custom structure
+  // size_t add_point(OMesh::Point point) {
+  //   // g_points.push_back(point);
+  //   points_handler.add_vertex(point);
+  //   return points_handler.n_vertices() - 1;  // Return the index of the added point
+  // }
 
-  // Add a vertex using a point from your custom structure
-  OpenMesh::VertexHandle add_vertex_from_point(size_t point_index) {
-    if (point_index >= points_handler.n_vertices()) {
-      throw std::out_of_range("Invalid point index for add_vertex_from_point()");
-    }
+  //// Add a vertex using a point from your custom structure
+  // OpenMesh::VertexHandle add_vertex_from_point(size_t point_index) {
+  //   if (point_index >= points_handler.n_vertices()) {
+  //     throw std::out_of_range("Invalid point index for add_vertex_from_point()");
+  //   }
 
-    // Use the point from the custom structure to add a vertex
-    return add_vertex(points_handler.points()[point_index]);
-  }
+  //  // Use the point from the custom structure to add a vertex
+  //  return add_vertex(points_handler.points()[point_index]);
+  //}
 };
 
 #endif  // CPP_MESHER_GMESH_H
--- src/openmesh/openmesh_utils.cpp
+++ src/openmesh/openmesh_utils.cpp
@@ -1,7 +1,7 @@
 #include "openmesh_utils.h"
 namespace NED::openmeshutils {
 
-void compute_normals(GMesh &mesh){
+void compute_normals(GMesh &mesh) {
   mesh.request_vertex_normals();
   mesh.request_face_normals();
   // let the mesh update the normals
@@ -10,17 +10,16 @@ void compute_normals(GMesh &mesh){
   mesh.release_face_normals();
 }
 
-void set_normals(GMesh &mesh, glm::vec3 normal){
+void set_normals(GMesh &mesh, glm::vec3 normal) {
   mesh.request_vertex_normals();
 
-  for(auto vh : mesh.vertices()){
+  for (auto vh : mesh.vertices()) {
     mesh.set_normal(vh, OpenMesh::Vec3f(normal.x, normal.y, normal.z));
   }
 }
 
 void triangulate(GMesh &mesh) {
-
-    // Iterate over all faces and triangulate
+  // Iterate over all faces and triangulate
   for (GMesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it) {
     if (mesh.is_valid_handle(*f_it)) {
       mesh.triangulate(*f_it);
@@ -34,79 +33,40 @@ void triangulate(GMesh &mesh) {
 GMesh openmesh_cube() {
   GMesh mesh;
 
-  auto pt0 = mesh.add_point(GMesh::Point(-1, -1, 1));
-  auto pt1 = mesh.add_point(GMesh::Point(1, -1, 1));
-  auto pt2 = mesh.add_point(GMesh::Point(1, 1, 1));
-  auto pt3 = mesh.add_point(GMesh::Point(-1, 1, 1));
-  auto pt4 = mesh.add_point(GMesh::Point(-1, -1, -1));
-  auto pt5 = mesh.add_point(GMesh::Point(1, -1, -1));
-  auto pt6 = mesh.add_point(GMesh::Point(1, 1, -1));
-  auto pt7 = mesh.add_point(GMesh::Point(-1, 1, -1));
+  auto pt0 = mesh.add_vertex(GMesh::Point(-1, -1, 1));
+  auto pt1 = mesh.add_vertex(GMesh::Point(1, -1, 1));
+  auto pt2 = mesh.add_vertex(GMesh::Point(1, 1, 1));
+  auto pt3 = mesh.add_vertex(GMesh::Point(-1, 1, 1));
+  auto pt4 = mesh.add_vertex(GMesh::Point(-1, -1, -1));
+  auto pt5 = mesh.add_vertex(GMesh::Point(1, -1, -1));
+  auto pt6 = mesh.add_vertex(GMesh::Point(1, 1, -1));
+  auto pt7 = mesh.add_vertex(GMesh::Point(-1, 1, -1));
 
-
-  auto v00 = mesh.add_vertex_from_point(pt0);
-  auto v01 = mesh.add_vertex_from_point(pt1);
-  auto v02 = mesh.add_vertex_from_point(pt2);
-  auto v03 = mesh.add_vertex_from_point(pt3);
-  mesh.add_face(v00, v01, v02, v03);
-
-  auto v10 = mesh.add_vertex_from_point(pt7);
-  auto v11 = mesh.add_vertex_from_point(pt6);
-  auto v12 = mesh.add_vertex_from_point(pt5);
-  auto v13 = mesh.add_vertex_from_point(pt4);
-  mesh.add_face(v10, v11, v12, v13);
-  
-  auto v20 = mesh.add_vertex_from_point(pt1);
-  auto v21 = mesh.add_vertex_from_point(pt0);
-  auto v22 = mesh.add_vertex_from_point(pt4);
-  auto v23 = mesh.add_vertex_from_point(pt5);
-  mesh.add_face(v20, v21, v22, v23);
-
-  auto v30 = mesh.add_vertex_from_point(pt2);
-  auto v31 = mesh.add_vertex_from_point(pt1);
-  auto v32 = mesh.add_vertex_from_point(pt5);
-  auto v33 = mesh.add_vertex_from_point(pt6);
-  mesh.add_face(v30, v31, v32, v33);
-
-  auto v40 = mesh.add_vertex_from_point(pt3);
-  auto v41 = mesh.add_vertex_from_point(pt2);
-  auto v42 = mesh.add_vertex_from_point(pt6);
-  auto v43 = mesh.add_vertex_from_point(pt7);
-  mesh.add_face(v40, v41, v42, v43);
-
-  auto v50 = mesh.add_vertex_from_point(pt0);
-  auto v51 = mesh.add_vertex_from_point(pt3);
-  auto v52 = mesh.add_vertex_from_point(pt7);
-  auto v53 = mesh.add_vertex_from_point(pt4);
-  mesh.add_face(v50, v51, v52, v53);
-  
-
+  mesh.add_face(pt0, pt1, pt2, pt3);  // Front face
+  mesh.add_face(pt7, pt6, pt5, pt4);  // Back face
+  mesh.add_face(pt0, pt4, pt5, pt1);  // Bottom face
+  mesh.add_face(pt2, pt6, pt7, pt3);  // Top face
+  mesh.add_face(pt0, pt3, pt7, pt4);  // Left face
+  mesh.add_face(pt1, pt5, pt6, pt2);  // Right face
 
   return mesh;
 }
 
-GMesh openmesh_square() 
-{
+GMesh openmesh_square() {
   GMesh mesh;
-  
 
-  auto pt0 = mesh.add_point(GMesh::Point(0, 0, 0));
-  auto pt1 = mesh.add_point(GMesh::Point(1, 0, 0));
-  auto pt2 = mesh.add_point(GMesh::Point(1, 1, 0));
-  auto pt3 = mesh.add_point(GMesh::Point(0, 1, 0));
-
-  auto v0 = mesh.add_vertex_from_point(pt0);
-  auto v1 = mesh.add_vertex_from_point(pt1);
-  auto v2 = mesh.add_vertex_from_point(pt2);
-  auto v3 = mesh.add_vertex_from_point(pt3);
+  auto pt0 = mesh.add_vertex(GMesh::Point(0, 0, 0));
+  auto pt1 = mesh.add_vertex(GMesh::Point(1, 0, 0));
+  auto pt2 = mesh.add_vertex(GMesh::Point(1, 1, 0));
+  auto pt3 = mesh.add_vertex(GMesh::Point(0, 1, 0));
 
   mesh.request_vertex_texcoords2D();
-  mesh.set_texcoord2D(v0, OpenMesh::Vec2f(0.0f, 0.0f));
-  mesh.set_texcoord2D(v1, OpenMesh::Vec2f(1.0f, 0.0f));
-  mesh.set_texcoord2D(v2, OpenMesh::Vec2f(1.0f, 1.0f));
-  mesh.set_texcoord2D(v3, OpenMesh::Vec2f(0.0f, 1.0f));
+  mesh.set_texcoord2D(pt0, OpenMesh::Vec2f(0.0f, 0.0f));
+  mesh.set_texcoord2D(pt1, OpenMesh::Vec2f(1.0f, 0.0f));
+  mesh.set_texcoord2D(pt2, OpenMesh::Vec2f(1.0f, 1.0f));
+  mesh.set_texcoord2D(pt3, OpenMesh::Vec2f(0.0f, 1.0f));
 
-  auto f_handle = mesh.add_face(v0, v1, v2, v3);
+  auto f_handle = mesh.add_face(pt0, pt1, pt2, pt3);
 
   return mesh;
 }
