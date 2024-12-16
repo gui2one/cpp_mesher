85f37ae341b731e5f98bdfa2f9e1602beb63f407
gui2one <guillaume.rouault.fx@gmail.com>
2024-12-16 02:04:41 +0100

    added VertexPropertyHVariant

--- src/openmesh/gmesh.h
+++ src/openmesh/gmesh.h
@@ -6,6 +6,9 @@
 template <typename T>
 using VertexPropH = OpenMesh::VPropHandleT<T>;
 
+using VertexPropertyHVariant =
+    std::variant<VertexPropH<float>, VertexPropH<int>, VertexPropH<std::string>, VertexPropH<OpenMesh::Vec3f>>;
+
 class GMesh : public OpenMesh::PolyMesh_ArrayKernelT<> {
  public:
   GMesh() : OpenMesh::PolyMesh_ArrayKernelT<>() {}
@@ -13,7 +16,7 @@ class GMesh : public OpenMesh::PolyMesh_ArrayKernelT<> {
  public:
   // an array of custom polymesh properties
 
-  std::vector<VertexPropH<OpenMesh::Vec3f>> vertex_props;
+  std::vector<VertexPropertyHVariant> vertex_prop_handles;
 };
 
 #endif  // CPP_MESHER_GMESH_H
